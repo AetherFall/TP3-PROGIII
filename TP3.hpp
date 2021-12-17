fb48539ef05a0816e3272fde1ee756a76c5607d1
AetherFall <1735971@etu.cegep-lanaudiere.qc.ca>
2021-12-17 07:07:08 -0500

    Leaks verification: Check!

--- TP3.hpp
+++ TP3.hpp
@@ -14,8 +14,6 @@
 
 #define pathFileSystem "..//saves//ntfs.xml"
 
-//TODO Verification : Selon le leaksReport3.txt --> Est-ce que les données en mémoire dit "Still reachable" sont considéré comme des fuites de mémoires?
-
 /**
     @author William-Benoit Lambert
     @code 1735971
